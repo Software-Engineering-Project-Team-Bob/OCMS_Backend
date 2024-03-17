3c576dc056a6d52dc52ee2a12cb45d9ef1121dbe
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-17 23:30:15 +0530

    adding the server.js and the mongo db uri with the env variable

--- server.js
+++ server.js
@@ -1,1 +1,55 @@
-mongodb+srv://sk:ocmsbackend@cluster0.avbav4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+const classroomRoutes = require('./routes/classroom');
+const authRoutes = require('./routes/auth');
+
+app.use('/classes', classroomRoutes);
+app.use('/auth', authRoutes);
+
+// if ( process.env.NODE_ENV === "production" || 1) { 
+//     app.use(express.static(path.join(__dirname, "../client/build"))); 
+//     app.get("*", (req, res) => { 
+//         res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')); 
+//     })
+// }
+
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.statusCode = 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    const status = err.statusCode || 500;
+    const message = err.message;
+    res.status(status).json({message: message});
+})
+
+const DB_URL = process.env.MONGO_DB_URI;
+mongoose.connect(DB_URL, 
+{ 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+})
+.then(result => {
+    app.listen(5000);
+    console.log("Server started at port 5000");
+    console.log("http://localhost:4000");
+})
+.catch(err => {
+    console.log(err);
+})
