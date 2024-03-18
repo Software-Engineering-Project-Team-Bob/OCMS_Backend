9107892d62e428111e98bf9dea8af66760fc2774
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:33:32 +0530

    making the submit assignment feature for the students to submmit the assignment file link , the database onnly stores the public file link to the system , for simplet purposes we will make the file availble for all the users so that it can be acceased as  apubliuc system, fo  future we can make a container by definif the class code . also  the featurr eis there tht th eowner of the classroomsubmission gets to see it

---
+++ controllers/class/submitAssignment.js
@@ -0,0 +1,25 @@
+exports.submitAssignment = (req, res, next) => {
+    const studentName = req.body.studentName;
+    const studentEmail = req.body.studentEmail;
+    const assignmentId = req.body.assignmentId;
+    const fileLink = req.body.fileLink;
+    const classCode = req.body.classCode;
+    const fileName = req.body.fileName;
+
+    const submission = new Submission({
+        studentName,
+        studentEmail,
+        fileLink,
+        assignmentId,
+        classCode,
+        fileName
+    })
+
+    submission.save()
+        .then(result => {
+            res.json({message: "Submission created successfully"});
+        })
+        .catch(err => {
+            next(err);
+        })
+}
