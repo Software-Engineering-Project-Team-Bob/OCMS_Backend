9033a0fd86a87f6ac0ab0fdce0d78f4733af7e0e
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:27:17 +0530

    making a set grade feature

---
+++ controllers/class/setGrade.js
@@ -0,0 +1,23 @@
+exports.setGrade = (req, res, next) => {
+    const submissionId = req.body.submissionId;
+    const grade = req.body.grade;
+    Submission.findById(submissionId)
+        .then(submission => {
+            if (!submission) {
+                const err = new Error("Submission not found.");
+                err.statusCode = 422;
+                next(err);
+            }
+            submission.grade = grade;
+            submission.save()
+                .then(result => {
+                    res.json({message: "Grade saved successfully."});
+                })
+                .catch(err => {
+                    next(err);
+                })
+        })
+        .catch(err => {
+            next(err);
+        })
+}
