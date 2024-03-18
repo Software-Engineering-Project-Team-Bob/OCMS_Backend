1b12fd41b9a825714047d3d9b7fb8e9bb0e2d9e3
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:26:14 +0530

    creating a get classrom featur also

--- controllers/class/getClassroom.js
+++ controllers/class/getClassroom.js
@@ -1,31 +1,17 @@
 
+exports.getClassroom = (req, res, next) => {
+    const classCode = req.body.classCode;
+    Classroom.findOne({classCode: classCode})
+        .then(classroom => {
+            if (!classroom) {
+                const err = new Error("Invalid classcode.");
+                err.statusCode = 422;
+                next(err);
+            }
 
-exports.getClassrooms = (req, res, next) => {
-    const type = req.body.type;
-    const userEmail = req.body.userEmail;
-    if (type === "owned") {
-        Classroom.find({adminEmail: userEmail})
-            .then(results => {
-                res.json(results);
-            }).catch(err => {
-                next(err);
-            })
-    } else if (type === "enrolled") {
-        User.findOne({email: userEmail})
-            .then(user => {
-                Classroom.find({classCode: user.classesEnrolled})
-                    .then(results => {
-                        res.json(results);
-                    })
-                    .catch(err => {
-                        next(err);
-                    })
-            }).catch(err => {
-                next(err);
-            })
-    } else {
-        const err = new Error("Invalid params");
-        err.statusCode = 422;
-        next(err);
-    }
+            res.json(classroom);
+        })
+        .catch(err => {
+            next(err);
+        })
 }
---
+++ controllers/class/getClassrooms.js
@@ -0,0 +1,31 @@
+
+
+exports.getClassrooms = (req, res, next) => {
+    const type = req.body.type;
+    const userEmail = req.body.userEmail;
+    if (type === "owned") {
+        Classroom.find({adminEmail: userEmail})
+            .then(results => {
+                res.json(results);
+            }).catch(err => {
+                next(err);
+            })
+    } else if (type === "enrolled") {
+        User.findOne({email: userEmail})
+            .then(user => {
+                Classroom.find({classCode: user.classesEnrolled})
+                    .then(results => {
+                        res.json(results);
+                    })
+                    .catch(err => {
+                        next(err);
+                    })
+            }).catch(err => {
+                next(err);
+            })
+    } else {
+        const err = new Error("Invalid params");
+        err.statusCode = 422;
+        next(err);
+    }
+}
