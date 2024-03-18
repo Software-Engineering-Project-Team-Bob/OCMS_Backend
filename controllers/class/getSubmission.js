522254f8d4d755ce5e8695e677bfa9138b7d5d4e
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:30:48 +0530

    making the get submission feature forthe teachers to fetch the assignment links of the students

---
+++ controllers/class/getSubmission.js
@@ -0,0 +1,18 @@
+exports.getSubmission = (req, res, next) => {
+    const assignmentId = req.body.assignmentId;
+    const userEmail = req.body.userEmail;
+    
+    Submission.findOne({studentEmail: userEmail, assignmentId: new ObjectId(assignmentId)})
+        .then(submission => {
+            if (!submission) {
+                const err = new Error("Submission not found.");
+                err.statusCode = 422;
+                next(err);
+            } else {
+                res.json(submission);
+            }
+        })
+        .catch(err => {
+            next(err);
+        })
+}
