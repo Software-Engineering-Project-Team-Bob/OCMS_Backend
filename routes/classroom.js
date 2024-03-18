55333c7f3295bb7e8377ccd928fcef807b562608
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:51:52 +0530

    completing the import statement s error of class room routes

--- routes/classroom.js
+++ routes/classroom.js
@@ -4,24 +4,42 @@ const router = express.Router();
 
 const isAuth = require('../middlewares/is-auth')
 
-const classroomController = require('../controllers/classroom');
+const  createClassroom = require('../controllers/classroom/createClassroom');
+const  getClassrooms = require('../controllers/classroom/getClassrooms');
+const  joinClassroom = require('../controllers/classroom/joinClassroom');
+const  getClassroom = require('../controllers/classroom/getClassroom');
+const  deleteClassroom = require('../controllers/classroom/deleteClassroom');
+const  createDiscussion = require('../controllers/classroom/createDiscussion');
+const  getDiscussions = require('../controllers/classroom/getDiscussions');
+const  createAssignment = require('../controllers/classroom/createAssignment');
+const  getAssignments = require('../controllers/classroom/getAssignments');
+const  getAssignment = require('../controllers/classroom/getAssignment');
+const  getReminders = require('../controllers/classroom/getReminders');
+const  getAttendees = require('../controllers/classroom/getAttendees');
+const  submitAssignment = require('../controllers/classroom/submitAssignment');
+const  getSubmission = require('../controllers/classroom/getSubmission');
+const  deleteSubmission = require('../controllers/classroom/deleteSubmission');
+const  getSubmissions = require('../controllers/classroom/getSubmissions');
+const  setGrade = require('../controllers/classroom/setGrade');
 
-router.post('/createClassroom', classroomController.createClassroom);
-router.post('/getClassrooms', isAuth, classroomController.getClassrooms);
-router.post('/joinClassroom', classroomController.joinClassroom);
-router.post('/getClassroom', classroomController.getClassroom);
-router.delete('/deleteClassroom', classroomController.deleteClassroom);
-router.post('/createDiscussion', classroomController.createDiscussion);
-router.post('/getDiscussions', classroomController.getDiscussions);
-router.post('/createAssignment', classroomController.createAssignment);
-router.post('/getAssignments', classroomController.getAssignments);
-router.post('/getAssignment', classroomController.getAssignment);
-router.post('/getReminders', classroomController.getReminders);
-router.post('/getAttendees', classroomController.getAttendees);
-router.post('/submitAssignment', classroomController.submitAssignment);
-router.post('/getSubmission', classroomController.getSubmission);
-router.delete('/deleteSubmission', classroomController.deleteSubmission);
-router.post('/getSubmissions', classroomController.getSubmissions);
-router.post('/setGrade', classroomController.setGrade);
+
+
+router.post('/createClassroom', createClassroom);
+router.post('/getClassrooms', isAuth, getClassrooms);
+router.post('/joinClassroom', joinClassroom);
+router.post('/getClassroom', getClassroom);
+router.delete('/deleteClassroom', deleteClassroom);
+router.post('/createDiscussion', createDiscussion);
+router.post('/getDiscussions', getDiscussions);
+router.post('/createAssignment', createAssignment);
+router.post('/getAssignments', getAssignments);
+router.post('/getAssignment', getAssignment);
+router.post('/getReminders', getReminders);
+router.post('/getAttendees', getAttendees);
+router.post('/submitAssignment', submitAssignment);
+router.post('/getSubmission', getSubmission);
+router.delete('/deleteSubmission', deleteSubmission);
+router.post('/getSubmissions', getSubmissions);
+router.post('/setGrade', setGrade);
 
 module.exports = router; 
