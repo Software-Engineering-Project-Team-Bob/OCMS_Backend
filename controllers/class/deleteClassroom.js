341dc680dc97e9d558e4ad0fbf846487b544a669
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:23:37 +0530

    making the delete classroom feature

---
+++ controllers/class/deleteClassroom.js
@@ -0,0 +1,45 @@
+
+exports.deleteClassroom = (req, res, next) => {
+    const classCode = req.body.classCode;
+    // console.log(classCode);
+    Classroom.findOneAndDelete({classCode: classCode})
+        .then(async classroom => {
+            if (!classroom) {
+                const err = new Error("ClassCode does not exists");
+                err.statusCode = 422;
+                next(err);
+            } 
+
+            classroom.members.forEach(async memberEmail => {
+                await User.findOne({email: memberEmail})
+                    .then(user => {
+                        if (user) {
+                            user.classesEnrolled = user.classesEnrolled.filter(classEnrolledCode => {
+                                return classEnrolledCode.toString() !== classCode;
+                            });
+
+                            user.classesOwned = user.classesOwned.filter(classOwnedCode => {
+                                return classOwnedCode.toString() !== classCode;
+                            });
+
+                            user.save();
+                        }
+                    })
+                    .catch(err => {
+                        next(err);
+                    })
+            })
+
+            Discussion.deleteMany({classCode: classCode})
+                .then(result => {
+                    res.json({message: "Classroom deleted successfully"});
+                })
+                .catch(err => {
+                    next(err);
+                })
+
+        })
+        .catch(err => {
+            next(err);
+        })
+}
