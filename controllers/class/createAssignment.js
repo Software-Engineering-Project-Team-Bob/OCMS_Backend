87a13d878f45f05a4fba89dacc76522682598573
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:38:19 +0530

    creaating an assignment ai to teachers availabiltiy

---
+++ controllers/class/createAssignment.js
@@ -0,0 +1,27 @@
+exports.createAssignment = (req, res, next) => {
+    // console.log(req.body);
+
+    const classCode = req.body.classCode;
+    const name = req.body.name;
+    const desc = req.body.desc;
+    const dueDate = req.body.dueDate;
+    const fileLink = req.body.fileLink;
+    const creatorEmail = req.body.creatorEmail;
+
+    const assignment = new Assignment({
+        classCode: classCode,
+        name: name,
+        desc: desc,
+        dueDate: dueDate,
+        fileLink: fileLink,
+        creatorEmail: creatorEmail
+    })
+
+    assignment.save()
+        .then(result => {
+            res.json({message: "Assignment created successfully"});
+        })
+        .catch(err => {
+            next(err);
+        })
+}
