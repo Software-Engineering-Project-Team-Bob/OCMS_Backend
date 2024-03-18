cfa7c42149ccdca903525e33e9ae29a37c38954c
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:37:16 +0530

    .

---
+++ controllers/class/getAssignments.js
@@ -0,0 +1,10 @@
+exports.getAssignments = (req, res, next) => {
+    const classCode = req.body.classCode;
+    Assignment.find({classCode: classCode}).sort({dueDate: 1})
+        .then(results => {
+            res.json(results);
+        })
+        .catch(err => {
+            next(err);
+        })
+}
