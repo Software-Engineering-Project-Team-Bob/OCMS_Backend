24a64139759d8d14694650f7406b0d2669329803
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-18 11:37:38 +0530

    .

---
+++ controllers/class/getAssignment.js
@@ -0,0 +1,16 @@
+exports.getAssignment = (req, res, next) => {
+    const assignmentId = req.body.assignmentId;
+    Assignment.findById(assignmentId)
+        .then(assignment => {
+            User.findOne({email: assignment.creatorEmail})
+                .then(user => {
+                    res.json({...assignment._doc, creatorName: user.name});
+                })
+                .catch(err => {
+                    next(err);
+                })
+        })
+        .catch(err => {
+            next(err);
+        })
+}
