a8c87b1f5075ed8270dea592e9fb0e7fe5a90f46
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-19 15:31:12 +0530

    made connected to database

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ const DB_URL = process.env.MONGO_DB_URI;
 const PORT= process.env.PORT || 5000;
 mongoose.connect(DB_URL)
 .then(result => {
+    console.log("Connected to database");
     app.listen(5000);
     console.log("Server started at port 5000");
     console.log("http://localhost:5000");
