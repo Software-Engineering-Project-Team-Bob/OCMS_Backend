284e29fe323cf660cef417dababb2afd10af65a7
Sumit Kumar <sumitkumar@kgpian.iitkgp.ac.in>
2024-04-15 00:32:24 +0530

    crete TA-3

--- controllers/classroom/createTAs.js
+++ controllers/classroom/createTAs.js
@@ -17,6 +17,7 @@ const createTAs=(req,res,next)=>{
             next(err);
         }
         classroom.TAs.push(TAs);
+        classroom.members.push(TAs);
         return classroom.save();
     })
     .then(result=>{
