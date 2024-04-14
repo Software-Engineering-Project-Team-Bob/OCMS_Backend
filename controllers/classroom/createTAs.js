e4bcc55d8c4f15a2e0fdc04cce84cf237741bbab
Sumit Kumar <sumitkumar@kgpian.iitkgp.ac.in>
2024-04-14 23:53:00 +0530

    crete TA-2

--- controllers/classroom/createTAs.js
+++ controllers/classroom/createTAs.js
@@ -4,26 +4,38 @@ const Classroom=require('../../models/classroom');
 const createTAs=(req,res,next)=>{
     const classCode=req.body.classCode;
     const TAs=req.body.TAs;
-    Classroom.findOne.updateOne({classCode:classCode},{$push:{TAs:TAs}})
+    Classroom.findOne({classCode:classCode})
     .then(classroom=>{
         if(!classroom){
-            const err=new Error("ClassCode does not exists");
-            err.statusCode=422;
+            const err = new Error("Classroom with given class code does not exists.");
+            err.statusCode = 403;
             next(err);
         }
-        TAs.forEach(async TAEmail=>{
-            await User.findOne({email:TAEmail})
-            .then(user=>{
-                if(user){
-                    user.classesEnrolled.push(classCode);
-                    user.save();
-                }
-            })
-            .catch(err=>{
-                next(err);
-            })
-        })
-        res.json({message:"TAs added successfully"});
+        if (classroom.TAs.indexOf(TAs) >= 0) {
+            const err = new Error("TAs already Enrolled.");
+            err.statusCode = 403;
+            next(err);
+        }
+        classroom.TAs.push(TAs);
+        return classroom.save();
+    })
+    .then(result=>{
+        return User.findOne({email:TAs});
+    })
+    .then(user=>{
+        if(user.classesOwned.indexOf(classCode)>=0){
+            const err=new Error("TAs cannot enroll in class created by themselves.");
+            err.statusCode=403;
+            next(err);
+        }
+        user.classesEnrolled.push(classCode);
+        return user.save();
+    })
+    .then(result=>{
+        res.json({message:"TAs added successfully!"});
+    })
+    .catch(err=>{
+        next(err);
     })
 }
 
